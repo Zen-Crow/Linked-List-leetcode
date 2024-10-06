fdbe262f4cc8645fdc6c0e000781abd362a74692
git-lab-user1 <gurovam184@gmail.com>
2024-10-07 01:21:01 +0300

    edit headers

--- C/convert_binary_number_in_a_linked_list_to_integer.c
+++ C/convert_binary_number_in_a_linked_list_to_integer.c
@@ -1,21 +1,39 @@
-#include <cstddef>
-#include <cstdio>
+#include <stddef.h>
+#include <stdlib.h>
+#include <stdio.h>
 
 
-struct ListNode {
+struct ListNode
+{
     int val;
     struct ListNode *next;
 };
 
-int getDecimalValue(struct ListNode* head) {
+int getDecimalValue(struct ListNode *head)
+{
     char result[100];
-    struct ListNode* cur = head;
+    struct ListNode *cur = head;
     int i = 0;
-    while(cur != NULL) {
+    while (cur != NULL)
+    {
         result[i] = cur->val + '0';
         cur = cur->next;
         i++;
     }
     result[i] = '\0';
     return strtol(result, NULL, 2);
+}
+
+int main()
+{
+    struct ListNode *head = malloc(sizeof(struct ListNode));
+    head->val = 1;
+    head->next = malloc(sizeof(struct ListNode));
+    head->next->val = 0;
+    head->next->next = malloc(sizeof(struct ListNode));
+    head->next->next->val = 1;
+    head->next->next->next = NULL;
+
+    printf("%d", getDecimalValue(head));
+    return 0;
 }
